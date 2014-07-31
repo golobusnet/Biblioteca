2d27cde8cb206b2ae7e1c411e9dac2cd9e512a47
root <william_esc@hotmail.com>
2014-07-31 16:49:07 -0300

    primeiro banco

---
+++ bilioteca.sql
@@ -0,0 +1,181 @@
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `editora`
+--
+
+DROP TABLE IF EXISTS `editora`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `editora` (
+  `idEditora` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idEditora`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `editora`
+--
+
+LOCK TABLES `editora` WRITE;
+/*!40000 ALTER TABLE `editora` DISABLE KEYS */;
+/*!40000 ALTER TABLE `editora` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emprestimo`
+--
+
+DROP TABLE IF EXISTS `emprestimo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emprestimo` (
+  `dtDevolucao` date DEFAULT NULL,
+  `dtEmprestimo` date DEFAULT NULL,
+  `isbnLivro` varchar(255) NOT NULL DEFAULT '',
+  `idUsuario` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`isbnLivro`,`idUsuario`),
+  KEY `FK7C49D82074824939` (`isbnLivro`),
+  KEY `FK7C49D8206782A964` (`idUsuario`),
+  CONSTRAINT `FK7C49D8206782A964` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  CONSTRAINT `FK7C49D82074824939` FOREIGN KEY (`isbnLivro`) REFERENCES `livro` (`isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emprestimo`
+--
+
+LOCK TABLES `emprestimo` WRITE;
+/*!40000 ALTER TABLE `emprestimo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `emprestimo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estoque`
+--
+
+DROP TABLE IF EXISTS `estoque`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estoque` (
+  `idEstoque` int(11) NOT NULL AUTO_INCREMENT,
+  `isbnLivro` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idEstoque`),
+  KEY `FKA9B2E9A774824939` (`isbnLivro`),
+  CONSTRAINT `FKA9B2E9A774824939` FOREIGN KEY (`isbnLivro`) REFERENCES `livro` (`isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estoque`
+--
+
+LOCK TABLES `estoque` WRITE;
+/*!40000 ALTER TABLE `estoque` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estoque` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livro`
+--
+
+DROP TABLE IF EXISTS `livro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `livro` (
+  `isbn` varchar(255) NOT NULL,
+  `autor` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `idCategoria` int(11) DEFAULT NULL,
+  `idEditora` int(11) DEFAULT NULL,
+  PRIMARY KEY (`isbn`),
+  KEY `FKFA6AD5859648DC30` (`idEditora`),
+  KEY `FKFA6AD585BEDDE36E` (`idCategoria`),
+  CONSTRAINT `FKFA6AD5859648DC30` FOREIGN KEY (`idEditora`) REFERENCES `editora` (`idEditora`),
+  CONSTRAINT `FKFA6AD585BEDDE36E` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livro`
+--
+
+LOCK TABLES `livro` WRITE;
+/*!40000 ALTER TABLE `livro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `livro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=520 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (518,'william furtado'),(519,'paulo roberto');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-07-31 16:46:27
