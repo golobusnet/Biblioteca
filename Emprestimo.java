9bc513ec6882c428ff1ebf7d40c14f5e7951705f
williamfurtado <william_esc@hotmail.com>
2014-08-14 17:36:11 -0300

    Create Emprestimo.java

---
+++ Emprestimo.java
@@ -0,0 +1,16 @@
+
+public class Emprestimo {
+
+	public void menuEmprestimo(){
+		
+		
+		System.out.println("Digite a ID do usuario");
+		System.out.println("Digite a ISBN do livro para emprestimo");
+		System.out.println("data do emprestimo:");
+		System.out.println("data para devolucao:");
+		
+			
+	}
+	
+	
+}
